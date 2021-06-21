d9249e1383c21d0edd80ce37c137b8de7dd64799
Murilo <murilopaulino@live.com>
2021-06-21 02:53:05 -0300

    preparação da funcionalidade de edição

--- editar.php
+++ editar.php
@@ -10,17 +10,16 @@ include_once 'includes/menu.inc.php';
 </div>
 
 <?php 
-    include_once 'banco_de_dados/conexao.php';
+include_once 'banco_de_dados/conexao.php';
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+$_SESSION['id'] = $id;
+$querySelect = $link->query("select * from tb_clientes where id='$id'");
 
-    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
-    $querySelect = $link->query("select * from tb_clientes where id='$id'");
-
-    while($registros = $querySelect->fetch_assoc()):
-        $id = $registros['id'];
-        $nome = $registros['nome'];
-        $email = $registros['email'];
-        $telefone = $registros['telefone'];
-    endwhile;
+while($registros = $querySelect->fetch_assoc()):
+    $nome = $registros['nome'];
+    $email = $registros['email'];
+    $telefone = $registros['telefone'];
+endwhile;
 ?>
 
 <!-- formulário de cadastro -->
@@ -35,27 +34,27 @@ include_once 'includes/menu.inc.php';
                 <!-- campo nome -->
                 <div class="input-field col s12">
                     <i class="material-icons prefix">account_circle</i>
-                    <input type="text" name="nome" id="nome" value = "<?php echo $nome ?>" maxlength="40" required autofocus>
+                    <input type="text" name="nome" id="nome" value="<?php echo $nome ?>" maxlength="40" required autofocus>
                     <label for="nome">Nome do(a) cliente</label>
                 </div>
 
                 <!-- campo email -->
                 <div class="input-field col s12">
                     <i class="material-icons prefix">email</i>
-                    <input type="email" name="email" id="email" value = "<?php echo $email ?>" maxlength="50" required>
+                    <input type="email" name="email" id="email" value="<?php echo $email ?>" maxlength="50" required>
                     <label for="email">E-mail do(a) cliente</label>
                 </div>
 
                 <!-- campo telefone -->
                 <div class="input-field col s12">
                     <i class="material-icons prefix">phone</i>
-                    <input type="tel" name="telefone" id="telefone" value = "<?php echo $telefone ?>" maxlength="15" required>
+                    <input type="tel" name="telefone" id="telefone" value="<?php echo $telefone ?>" maxlength="15" required>
                     <label for="telefone">Telefone do(a) cliente</label>
                 </div>
 
                 <!-- botões -->
                 <div class="input-field col s12">
-                    <input type="reset" value="alterar" class="btn blue"> 
+                    <input type="submit" value="alterar" class="btn blue"> 
                     <a href="consultas.php" class="btn red">Cancelar</a>
                 </div>
             </fieldset>
